add full brain simuluation with 2 eigs order 2 and corresponding leadfield with randomized gc connections. fix plotting functions to respect new api. clean up link matrix computation code in _roi and clean up stats code in _plotting. fix bug in _stats that overwrote link matrix values. add trimmed mean to _stats. add t tests back to _stats.

_plotting.py
@@ -188,9 +188,9 @@ def heatmap(self, condition1, condition2, status1='C1', status2='C2', hemi=False
     ###################################
     if hemi == False:
         if norm:
-            c1_group_avg, c2_group_avg = self.group_norm_averages_whole(condition1, condition2)
+            c1_group_avg, c2_group_avg = self.group_whole_average(condition1, condition2, matnorm=True)
         else:
-            c1_group_avg, c2_group_avg = self.group_averages_whole(condition1, condition2)
+            c1_group_avg, c2_group_avg = self.group_whole_average(condition1, condition2, matnorm=False)
 
         self.heatmap_linkmatrix(c1_group_avg, c2_group_avg, status1, status2, hemi, 
                      overlay_nums, vmin, vmax, diffvmin, diffvmax, figsize, hemimapping, 
@@ -198,13 +198,14 @@ def heatmap(self, condition1, condition2, status1='C1', status2='C2', hemi=False
         return c1_group_avg, c2_group_avg
     else:
         if norm:
-            c1_group_avg_hemi, c2_group_avg_hemi = self.group_norm_averages_hemi(condition1, condition2)
+            c1_group_avg_hemi, c2_group_avg_hemi = self.group_hemi_average(condition1, condition2, matnorm=True)
         else:
-            c1_group_avg_hemi, c2_group_avg_hemi = self.group_averages_hemi(condition1, condition2)
+            c1_group_avg_hemi, c2_group_avg_hemi = self.group_hemi_average(condition1, condition2, matnorm=False)
 
         self.heatmap_linkmatrix(c1_group_avg_hemi, c2_group_avg_hemi, status1, status2, hemi, 
                      overlay_nums, vmin, vmax, diffvmin, diffvmax, figsize, hemimapping, 
                      cbar1scaling, cbar2scaling, rotate_labels)
+
         return c1_group_avg_hemi, c2_group_avg_hemi
 
 
@@ -215,7 +216,7 @@ def heatmap(self, condition1, condition2, status1='C1', status2='C2', hemi=False
 def circle_plot(self, condition1, condition2, status1='C1', status2='C2', hemi=False):
     ###################################
     if hemi == False:
-        c1_group_avg, c2_group_avg = self.group_averages_whole(condition1, condition2)
+        c1_group_avg, c2_group_avg = self.group_whole_average(condition1, condition2)
         
         
         fig, ax = plt.subplots(1, 3, figsize=(10, 3))
@@ -235,7 +236,7 @@ def circle_plot(self, condition1, condition2, status1='C1', status2='C2', hemi=F
 
     ###################################
     else:
-        c1_group_avg_hemi, c2_group_avg_hemi = self.group_averages_hemi(condition1, condition2)
+        c1_group_avg_hemi, c2_group_avg_hemi = self.group_hemi_average(condition1, condition2)
             
         
         fig, ax = plt.subplots(2, 6, figsize=(20, 7))
@@ -341,7 +342,7 @@ def _add_connection(ax, posa, posb, bidirectional=False, inner=True, arc=0.2,
     mid_dx, mid_dy = _push_wedge(pushfactor['inner'], mid_dx, mid_dy, centerxy)
     midx, midy = _push_wedge(pushfactor['inner'], midx, midy, centerxy)
 
-    ax.arrow(midx, midy, 0.001*(mid_dx-midx), 0.001*(mid_dy-midy), width=0.01, color=color['inner'])
+#     ax.arrow(midx, midy, 0.001*(mid_dx-midx), 0.001*(mid_dy-midy), width=0.01, color=color['inner'])
     
     if bidirectional:
         arrow = mpatches.FancyArrowPatch(posa, posb,
@@ -350,7 +351,7 @@ def _add_connection(ax, posa, posb, bidirectional=False, inner=True, arc=0.2,
         mid_dx, mid_dy, midx, midy = _trace_bezier_midpoint(arrow)
         mid_dx, mid_dy = _push_wedge(pushfactor['outer'], mid_dx, mid_dy, centerxy)
         midx, midy = _push_wedge(pushfactor['outer'], midx, midy, centerxy)
-        ax.arrow(midx, midy, 0.001*(midx-mid_dx), 0.001*(midy-mid_dy), width=0.01, color=color['outer'])
+#         ax.arrow(midx, midy, 0.001*(midx-mid_dx), 0.001*(midy-mid_dy), width=0.01, color=color['outer'])
         
 
 def _draw_selfloop_arrow(ax, centX, centY, radius, angle_, theta2_, color_):
@@ -396,20 +397,24 @@ def railroad_plot(lkm, rad=0.3, centerxy=(0.5, 0.5), nverts=3, region=['P','F', 
 
     xs, ys, _, _ = _get_vertices(nverts, rad, 0, centerxy)
     xs_selfloop, ys_selfloop, _, thetas_selfloop = _get_vertices(nverts, rad+0.1, 0, centerxy)
-
-    for i in range(nverts):
-        a = lkm[i, (i+1)%nverts]
-        b = lkm[(i+1)%nverts, i]
+    
+    # create links in order of darkness (lightest first)
+    is_, js_ = np.unravel_index(np.argsort(lkm, axis=None), (nverts,nverts))
+    
+    for i, j in zip(is_, js_):
+        a = lkm[i, j]
+        b = lkm[j, i]
         assert(a <= 1 and a >= 0 and b <= 1 and b >= 0)
         if a == 1:
             a = a - 0.00001
         if b == 1:
             b = b - 0.00001
 
+
         color_i = cmap(a)
         color_o = cmap(b)
-        _add_connection(ax, (xs[i], ys[i]), (xs[(i+1)%nverts], ys[(i+1)%nverts]), 
-                    bidirectional=True, arc=0.25, pushfactor={'inner':0.715, 'outer':1.147}, 
+        _add_connection(ax, (xs[i], ys[i]), (xs[j], ys[j]), 
+                    bidirectional=True, arc=0.25, pushfactor=pushfactor, 
                     color={'inner':color_i, 'outer':color_o})
 
 

_roi.py
@@ -6,6 +6,7 @@ import pickle
 import pandas as pd
 # from tabulate import tabulate
 import matplotlib.pyplot as plt
+import eelbrain as eel
 
 
 @staticmethod
@@ -68,25 +69,17 @@ def ico_to_desikan(rois, roi_names):
     df = pd.DataFrame(A)
     df = df[roi_names]
 
-    return df #B, transform, weights, B_noise
+    return df
 
 
 @staticmethod
-def gen_noise_models(df, J, noise_model_seed, n_noise_models, noise_model_dim):
-    if not noise_model_seed:
-        noise_model_seed = 0
-    B_noise = np.zeros((n_noise_models, noise_model_dim, noise_model_dim))
-
-    for nmi in range(n_noise_models):
-        df_noise = df.copy()
-        # increment random state by 1 each noise model run for reproducibility
-        df_noise = df_noise.groupby(df_noise.index >= len(df_noise)//2)\
-            .sample(frac=1, random_state=noise_model_seed+nmi)
-        df_noise = df_noise.reset_index(drop=True)
-        B_n, _, w_n = transform_and_weight(df_noise, J)
-        B_noise[nmi] = np.nan_to_num(B_n/w_n)
-
-    B_noise = np.mean(B_noise, axis=0)
+def gen_noise_models(J):
+    B_noise = partition(np.ones_like(J))
+    hemi_noise_floor = partition(J).mean(axis=1).mean(axis=1)
+    for i in range(4):
+        B_noise[i] *= hemi_noise_floor[i] 
+    
+    B_noise = unpartition(B_noise)
 
     return B_noise
 
@@ -124,6 +117,24 @@ def get_roi_mapping(subject, src_target, subjects_dir, target_lobes, lobe_mappin
 
     return rois, roi_names
 
+@staticmethod
+def morph_symmetrical(J, src, subjects_dir):
+    src = eel.SourceSpace.from_mne_source_spaces(src, 'ico-1', subjects_dir, parc='aparc')
+
+    # connectivity matrices ought to be of shape (source space, source space), but eelbrain
+    # doesn't allow for that. instead, we create a dummy dimension (case, source space) and 
+    # transform only the latter to obtain (case, transformed source space). Then transpose 
+    # case and source space dimensions and transform the latter axis again. The result is
+    # of shape (transformed source space, transformed source space) which is still 84x84 
+    # but with entries morphed so that left and right hemispheres are symmetrical.
+    ndv = eel.NDVar(J, dims=(eel.Case, src))
+    ndvs = eel.xhemi(ndv)
+    arr_half = np.concatenate([ndvs[0].x, ndvs[1].x], axis=1).T
+    ndv = eel.NDVar(arr_half, dims=(eel.Case, src))
+    ndvs = eel.xhemi(ndv)
+    arr_corrected = np.concatenate([ndvs[0].x, ndvs[1].x], axis=1).T
+    return arr_corrected
+
 
 def construct_link_matrix(self):
     self.conn_raw = dict()
@@ -131,29 +142,42 @@ def construct_link_matrix(self):
     self.conn_hemi_raw = dict()
     self.conn_hemi = dict()
 
-    for subject_vec in [['fsaverage'], self.CONTROLS, self.PATIENTS]:
+    for subject_vec in [['fsaverage', 'fsaverage_sym'], self.CONTROLS, self.PATIENTS]:
         for subject in subject_vec:
             if subject == 'fsaverage':
                 self.experiment.e.set(subject=subject, match=False)
                 src_target = self.experiment.e.load_src(src='ico-1')
                 rois, roi_names = get_roi_mapping(subject, src_target, self.subjects_dir, self.target_lobes, self.lobe_mapping)
                 df = ico_to_desikan(rois, roi_names)
-
+                
+                
                 # 68 x 84 (ico->anatomical)
                 self.fsaverage_anat_morph = df.T.values
 
                 # 84 x 68 (anatomical->ico)
                 self.fsaverage_ico_morph = np.linalg.pinv(self.fsaverage_anat_morph)
                 continue
-            
-            
+                
+            if subject  == 'fsaverage_sym':
+                self.experiment.e.set(subject=subject, match=False)
+                src_target = self.experiment.e.load_src(src='ico-1')
+                rois, roi_names = get_roi_mapping(subject, src_target, self.subjects_dir, self.target_lobes, self.lobe_mapping)
+                df = ico_to_desikan(rois, roi_names)
+                
+                # 68 x 84 (ico->anatomical)
+                self.fsaverage_sym_anat_morph = df.T.values
+
+                # 84 x 68 (anatomical->ico)
+                self.fsaverage_sym_ico_morph = np.linalg.pinv(self.fsaverage_sym_anat_morph)
+                continue
+      
             self.experiment.e.set(subject=subject)
 
-            src_target = self.experiment.e.load_src(src='ico-1')
-
-            rois, roi_names = get_roi_mapping(subject, src_target, self.subjects_dir, self.target_lobes, self.lobe_mapping)
-
             for visit_idx, visit in enumerate(self.visits):
+                self.experiment.e.set(visit=visit)
+                src_target = self.experiment.e.load_src(src='ico-1')
+                rois, roi_names = get_roi_mapping(subject, src_target, self.subjects_dir, self.target_lobes, self.lobe_mapping)
+                
                 for session_idx, session in enumerate(self.sessions):
                     for trial_idx, trial in enumerate(self.trials):
                         for file_pattern in self.file_patterns:
@@ -179,289 +203,37 @@ def construct_link_matrix(self):
                                 J[:] = np.nan
                                 NLGC_obj = None
 
+                            J_sym = morph_symmetrical(J, src_target, self.subjects_dir)
 
                             df = ico_to_desikan(rois, roi_names)
                             B, transform, weights = transform_and_weight(df, J)
-                            B_noise = gen_noise_models(df, J, self.noise_model_seed, self.n_noise_models, len(roi_names))
+                            J_noise = gen_noise_models(J)
+                            B_noise, _, _ = transform_and_weight(df, J_noise)
 
                             if subject in self.reflect_subs:
                                 B = unpartition(reflect(partition(B)))
                                 B_noise = unpartition(reflect(partition(B_noise)))
                                 weights = unpartition(reflect(partition(weights)))
-                                # TODO fill this in later
-                                transform = None 
+                                part_nlabels = len(transform)//2
+                                
+                                # swap top and bottom halves
+                                transform = np.concatenate([transform[part_nlabels:, :], transform[:part_nlabels, :]], axis=0) 
+                               
+                                # swap operation should yield the same weight matrix
+                                assert(np.all((transform.sum(axis=1)[:, np.newaxis] @ transform.sum(axis=1)[:, np.newaxis].T) == weights))
 
                             self.models.append([file_name, f"{subject}{visit}{session}{trial}", NLGC_obj])
                             self.models_transform.append([subject, visit, session, trial, file_name, NLGC_obj, 
                                                           B, np.nan_to_num(B/weights), transform, weights, 
                                                           rois, roi_names, 
-                                                          B_noise])
-
+                                                          np.nan_to_num(B_noise/weights), J, J_sym, df])
 
                             self.conn_hemi_raw[f'{subject}{visit}{session}{trial}'] = partition(B)
-                            self.conn_raw[f'{subject}{visit}{session}{trial}'] = partition(B).mean(axis=0)
-
                             self.conn_hemi[f'{subject}{visit}{session}{trial}'] = partition(B/weights)
-                            self.conn[f'{subject}{visit}{session}{trial}'] = partition(B/weights).mean(axis=0)
-
-                            
-
 
     self.models_transform = pd.DataFrame(self.models_transform)
     self.models_transform.columns = ['subject', 'visit', 'session', 'trial', 
               'file_name', 'NLGC_obj', 'B', 'B_norm', 'transform',
-              'weights', 'rois', 'roi_names', 'B_noise']
-
-
-
-
-# def construct_link_matrix(self):
-#     link_count = dict() 
-#     lobe_weights = dict()
-
-#     for subject_vec in [self.CONTROLS, self.PATIENTS]:
-#         for subject in subject_vec:
-#             self.experiment.e.set(subject=subject)
-
-#             src_target = self.experiment.e.load_src(src='ico-1')
-
-#             try:
-#                 labels = mne.read_labels_from_annot(subject, parc='aparc', subjects_dir=self.subjects_dir)
-#                 label_names = [label.name for label in labels]
-
-#                 lobe_labels = []
-
-#                 for lobe_idx, lobe_ in enumerate(self.target_lobes):
-#                     for hemi_idx, hemi in enumerate(['lh', 'rh']):
-#                         temp_label = []
-#                         for ROI_idx, ROI in enumerate(self.lobe_mapping[lobe_]):
-#                             if ROI_idx == 0:
-#                                 temp_label = labels[label_names.index(f"{ROI}-{hemi}")]
-#                             else:
-#                                 temp_label += labels[label_names.index(f"{ROI}-{hemi}")]
-#                         temp_label.name = f"{lobe_}-{hemi}"
-#                         lobe_labels.append(temp_label)
-
-#             except Exception as e:
-#                 # check the mri/label folder for .lh and .rh parcellation files
-#                 print(f"Ooops! Label issue with {subject}: {e}") 
-#                 continue
-
-#             for lobe_src, lobe_tar in itertools.product(self.target_lobes, repeat=2):
-#                 link_count[f"{subject};{lobe_src}->{lobe_tar}"] = np.zeros((len(self.visits), len(self.sessions), len(self.trials), 4))
-#             link_count[f"{subject};total"] = np.zeros((len(self.visits), len(self.sessions), len(self.trials), 4))
-
-#             for visit_idx, visit in enumerate(self.visits):
-#                 for session_idx, session in enumerate(self.sessions):
-#                     for trial_idx, trial in enumerate(self.trials):
-#                         for file_pattern in self.file_patterns:
-#                             file_name = eval(f"f'{file_pattern}'")
-#                             try:
-#                                 with open(file_name, 'rb') as fp: 
-#                                     NLGC_obj = pickle.load(fp)
-
-#                                 J = NLGC_obj.get_J_statistics()
-#                                 if J.sum() < self.minlinkcount or J.sum() > self.maxlinkcount:
-#                                     raise ValueError(f"link count {J.sum()} outside of analysis range {self.minlinkcount}-{self.maxlinkcount}!")
-                                
-#                                 self.models.append([file_name, f"{subject}{visit}{session}{trial}", NLGC_obj])
-
-                                
-#                             except Exception as e:
-#                                 print(f"caught exception: {e}")
-#                                 print(f"continuing with zero link matrix for {subject}, v={visit}, s={session}, t={trial}")
-#                                 self.models.append([file_name, f"{subject}{visit}{session}{trial}", None])
-#                                 J = np.zeros([84, 84])
-#                             this_visit_link_count = get_num_links(J.copy(), self.target_lobes, src_target, 
-#                                                                     lobe_labels, normalization=False)
-#                             for key, item in this_visit_link_count.items():
-#                                 link_count[f"{subject};{key}"][visit_idx][session_idx][trial_idx] = item    
-#                                 link_count[f"{subject};total"][visit_idx][session_idx][trial_idx] += item
-#     self.link_count = link_count
-#     self.pairwise_rois = list(this_visit_link_count.keys())
-    
-#     return self.link_count
-
-
-# def tabulate_links(self, percentages=True, verbose=True):
-#     if percentages:
-#         if verbose:
-#             print("\n These are <<percentages>> \n \n")
-#     else:
-#         if verbose:
-#             print("\n These are <<raw link counts>> \n \n")
-
-#     group_table = []
-#     conn = dict()
-#     conn_hemi = dict()
-#     conn_raw = dict()
-#     conn_hemi_raw = dict()
-
-#     for subject_vec in [self.CONTROLS, self.PATIENTS]:
-#         link_tables = []
-
-#         for visit_idx, visit in enumerate(self.visits):
-#             for session_idx, session in enumerate(self.sessions):
-#                 for trial_idx, trial in enumerate(self.trials):
-#                     this_table = []
-#                     first_row = []
-
-#                     first_row.append(f"visit={visit}")
-#                     for subject in subject_vec:
-#                         first_row.append(subject)
-#                     this_table.append(first_row)
-
-#                     for key in self.pairwise_rois:
-#                         this_row = []
-#                         this_row.append(key)
-#                         this_subject_total = 0
-#                         for subject in subject_vec:
-#                             if verbose >= 2:
-#                                 print(subject_vec, visit_idx, session_idx, trial_idx)
-#                             this_link_count = self.link_count[f"{subject};{key}"][visit_idx][session_idx][trial_idx].sum() 
-
-#                             # this_con = np.zeros((len(target_lobes), len(target_lobes)))
-#                             # ll, rr, lr, rl
-#                             this_con_hemi = np.zeros((4, len(self.target_lobes), len(self.target_lobes)))
-#                             this_con_hemi_raw = np.zeros((4, len(self.target_lobes), len(self.target_lobes)))
-#                             for j, source in enumerate(self.target_lobes):
-#                                 for i, target in enumerate(self.target_lobes):
-#                                     if verbose >= 3:
-#                                         print(f'checking link {j}->{i}')
-#                                     if self.link_count[f"{subject};total"][visit_idx][session_idx][trial_idx].sum() != 0:
-#                                         this_con_hemi[:, i, j] = \
-#                                         self.link_count[f"{subject};{source}->{target}"][visit_idx][session_idx][trial_idx]\
-#                                         / self.link_count[f"{subject};total"][visit_idx][session_idx][trial_idx].sum()
-#                                         this_con_hemi_raw[:, i, j] = \
-#                                         self.link_count[f"{subject};{source}->{target}"][visit_idx][session_idx][trial_idx]
-
-#                                 conn[f'{subject}{visit}{session}{trial}'] = this_con_hemi.sum(axis=0)
-#                                 conn_raw[f'{subject}{visit}{session}{trial}'] = this_con_hemi_raw.sum(axis=0)
-#                                 conn_hemi[f'{subject}{visit}{session}{trial}'] = this_con_hemi
-#                                 conn_hemi_raw[f'{subject}{visit}{session}{trial}'] = this_con_hemi_raw
-
-#                             if percentages:
-#                                 if self.link_count[f"{subject};total"][visit_idx][session_idx][trial_idx].sum() != 0:
-#                                     this_link_count /= self.link_count[f"{subject};total"][visit_idx][session_idx][trial_idx].sum()
-#                                 this_row.append(round(this_link_count*100, 1))
-#                             else:
-#                                 this_row.append(round(this_link_count, 2))
-#                         this_table.append(this_row)
-#                     link_tables.append(this_table)
-#         group_table.append(link_tables)
-
-#     for group_idx, subject_vec in enumerate([self.CONTROLS, self.PATIENTS]):
-#         status = 'patient' if subject_vec[0] in self.PATIENTS else 'control'
-#         if verbose:
-#             print('*'+status+'*\n')
-#         for visit_idx, visit in enumerate(self.visits):
-#             for session_idx, session in enumerate(self.sessions):
-#                 if verbose:
-#                     print(tabulate(group_table[group_idx][visit_idx], headers='firstrow'))
-#                     print('\n\n')
-#         if verbose:
-#             print('\n\n\n')
-#     self.conn_hemi = conn_hemi
-#     self.conn_hemi_raw = conn_hemi_raw
-#     self.conn = conn
-#     self.conn_raw = conn_raw
-#     return group_table
-
-
-# def desikan_to_ico(self, region, lobe_mapping, verbose=False, subject='fsaverage'):
-#     labels = mne.read_labels_from_annot(subject, parc='aparc', subjects_dir=self.subjects_dir)
-#     label_names = [label.name for label in labels]
-
-#     lobe_labels = []
-#     target_lobes = list(lobe_mapping.keys())
-
-#     for lobe_idx, lobe_ in enumerate(target_lobes):
-#         for hemi_idx, hemi in enumerate(['lh', 'rh']):
-#             temp_label = []
-#             for ROI_idx, ROI in enumerate(lobe_mapping[lobe_]):
-#                 if ROI_idx == 0:
-#                     temp_label = labels[label_names.index(f"{ROI}-{hemi}")]
-#                 else:
-#                     temp_label += labels[label_names.index(f"{ROI}-{hemi}")]
-#             temp_label.name = f"{lobe_}-{hemi}"
-#             lobe_labels.append(temp_label)
-
-#     link_count = dict()
-#     for lobe_src, lobe_tar in itertools.product(target_lobes, repeat=2):
-#         link_count[f"{lobe_src}->{lobe_tar}"] = np.zeros(4)
-    
-#     if subject == 'fsaverage':
-#         self.experiment.e.set(subject=subject, match=False)
-#     else:
-#         self.experiment.e.set(subject=subject)
-#     src_target = self.experiment.e.load_src(src='ico-1')
-
-#     patch_to_labels = patch_to_ROI(src_target, lobe_labels)
-#     idxs = []
-#     for idx, i in enumerate(patch_to_labels):
-#         # this patch straddles two or more regions, 
-#         # don't consider it for the sake of simplicity
-#         if i[0][1] != 1:
-#             if verbose:
-#                 print("skipping", idx, i, "straddles regions")
-#             continue
-
-#         # only want indices for this region
-#         if i[0][0].split('-')[0] != region:
-#             continue
-#         if verbose:
-#             print(idx, i)
-#         idxs.append(idx)
-#     return idxs
-
-
-def lkm_to_matrix(self, lkm, verbose=False):
-    d = dict()
-    for reg in list(self.lobe_mapping.keys()):
-        idxs = self.get_patch_idxs_in_region("fsaverage", reg, self.lobe_mapping, verbose)
-        d[reg] = idxs
-
-    A = np.zeros((84,84))
-
-
-    for i in range(84):
-        for j in range(84):
-            if i < 42:
-                hemi1 = 'lh'
-            else:
-                hemi1 = 'rh'
-            if j < 42:
-                hemi2 = 'lh'
-            else:
-                hemi2 = 'rh'
-
-            reg1 = None
-            reg2 = None
-            for reg_idx, reg in enumerate(list(self.lobe_mapping.keys())):
-                if i in d[reg]:
-                    reg1 = reg_idx
-                if j in d[reg]:
-                    reg2 = reg_idx
-
-            if reg1 is None or reg2 is None:
-                continue
-
-            idx = self._get_hemi_idx(hemi1, hemi2)
-            val = lkm[idx][reg2, reg1]
-
-            A[j, i] = val
-
-    
-    remap = []
-    for key in list(self.lobe_mapping.keys()):
-        remap.extend(d[key])
-    remap = remap + list(np.setdiff1d(list(range(84)), remap))
-    
-    if verbose:
-        plt.imshow(A)
-        plt.show()
-        plt.imshow(A[remap, :][:, remap])
-        plt.show()
-    return A, remap
+              'weights', 'rois', 'roi_names', 'B_noise', 'J', 'J_sym', 'ico_to_desikan']
         
 